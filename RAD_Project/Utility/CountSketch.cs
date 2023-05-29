25859d2cf074885aee583753020246bc16025903
Henrik Christensen <henrikdchristensen@gmail.com>
2023-05-29 13:36:02 +0200

    rename

--- RAD_Project/Utility/CountSketch.cs
+++ RAD_Project/Utility/CountSketch.cs
@@ -1,16 +1,17 @@
 using Utility;
 
-// Opgave 5. Implementering af hashfunktioner til Count-Sketch
-public class ModHash
+public class CountSketch
 {
     private readonly IHashing g;
     private readonly int t;
     private readonly int b = 89;
-    public ModHash(IHashing g, int t)
+    public CountSketch(IHashing g, int t)
     {
         this.g = g;
         this.t = t;
     }
+
+    // Opgave 5. Implementering af hashfunktioner til Count-Sketch
     public (ulong, ulong) Hash(ulong x, int l)
     {
         /*
@@ -22,10 +23,20 @@ public class ModHash
             Her er p = 2^b − 1 = 2^89 − 1, så b = 89. Med andre ord består h(x) af de log_2(m) = t
             mindst betydende bits af g(x) og s(x) er enten −1 eller 1 afhængigt af værdien af den mest
             betydende bit i g(x).
-            Til implementeringsdetaljerne skal I bruge Algoritme 2 i noterne om second moment estimation.
+            Til implementeringsdetaljerne skal Algoritme 2 i noterne om second moment estimation benyttes.
         */
         ulong y = g.Hash(x, t);
         return (y & ((1UL << l) - 1UL), // h(x)
                 y >> (b - 1) << 1); // s(x)
+    }
+
+    // Opgave 6. Implementering af Count-Sketch
+    public ulong Apply(ulong x, int l)
+    {
+        /*
+            Count-Sketch, der er parametriseret ved hashfunktioner h : U → [m] og s : U → {−1,1}, hvor m = 2^t er en toerpotens.
+            Samt en funktion, der givet sketch C[0,...,m−1] udregner estimatet X = ∑_{y∈[m]} C[y]^2 for S.
+        */
+        return 1;
     }
 }
